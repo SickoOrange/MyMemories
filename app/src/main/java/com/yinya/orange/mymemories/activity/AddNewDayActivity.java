954aee1aa2300a413725a2fc2a9cbc5be1c2e760
SickoOrange <181536584@qq.com>
2017-07-16 18:03:23 +0100

    4. commit

--- app/src/main/java/com/yinya/orange/mymemories/activity/AddNewDayActivity.java
+++ app/src/main/java/com/yinya/orange/mymemories/activity/AddNewDayActivity.java
@@ -7,19 +7,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.ImageViewTarget;
 import com.yinya.orange.mymemories.R;
 
-public class AddNewDayActivity extends AppCompatActivity {
+public class AddNewDayActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_day);
+
+        initView();
+
+    }
+
+    private void initView() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("add new countdown");
+        toolbar.setTitle("Add New Anniversary");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -27,6 +34,34 @@ public class AddNewDayActivity extends AppCompatActivity {
 
         Glide.with(this).load("http://yesofcorsa.com/wp-content/uploads/2016/12/4k-Love-Wallpaper-Download.jpg")
                 .fitCenter().into(img);
+
+        TextView tv_title = (TextView) findViewById(R.id.tv_title);
+        TextView tv_description = (TextView) findViewById(R.id.tv_description);
+        TextView tv_data_from = (TextView) findViewById(R.id.tv_data_from);
+        TextView tv_data_to = (TextView) findViewById(R.id.tv_data_to);
+
+        ImageView img_edit_title = (ImageView) findViewById(R.id.img_edit_title);
+        ImageView img_edit_description = (ImageView) findViewById(R.id.img_edit_description);
+        ImageView img_edit_data_from = (ImageView) findViewById(R.id.img_edit_data_from);
+        ImageView img_edit_data_to = (ImageView) findViewById(R.id.img_edit_data_to);
+
+
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.img_edit_title:
+                break;
+            case R.id.img_edit_description:
+                break;
+            case R.id.img_edit_data_from:
+                break;
+            case R.id.img_edit_data_to:
+                break;
+
+        }
+
+
+    }
 }
